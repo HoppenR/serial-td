33b31a64ed78f823b9225e6cd96acf02de0b259e
benkliator <benjaminekerna@gmail.com>
2024-09-15 13:53:09 +0200

    add collision

--- serial-td/kit/kit.gd
+++ serial-td/kit/kit.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 var baseTower = preload("res://tower/base_tower.tscn")
+@onready var tileMap = $"../PlayerTraversal"
 
-const speed: float = 300.0
+
 var inputDirection: Vector2
 var moving: bool = false
 var tileSize: int = 16
@@ -16,23 +17,34 @@ func _physics_process(delta: float) -> void:
 
 	# TODO(Hop): Make sure movement is /somewhat/ aligned to the grid?
 	inputDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
 	if Input.is_action_pressed("ui_down"):
-		inputDirection = Vector2(0,1)
+		inputDirection = Vector2.DOWN
 		_move()
 	if Input.is_action_pressed("ui_up"):
-		inputDirection = Vector2(0,-1)
+		inputDirection = Vector2.UP
 		_move()
 	if Input.is_action_pressed("ui_left"):
-		inputDirection = Vector2(-1,0)
+		inputDirection = Vector2.LEFT
 		_move()
 	if Input.is_action_pressed("ui_right"):
-		inputDirection = Vector2(1,0)
+		inputDirection = Vector2.RIGHT
 		_move()
 
 	move_and_slide()
 	
 func _move() -> void:
 	if inputDirection:
+		var currentTile: Vector2i = tileMap.local_to_map(global_position)
+		var targetTile: Vector2i = Vector2i(
+			currentTile.x + inputDirection.x,
+			currentTile.y + inputDirection.y,
+		)
+		var tileData: TileData = tileMap.get_cell_tile_data(targetTile)
+		
+		if tileData.get_custom_data("walkable") == false:
+			return
+			
 		if not moving:
 			moving = true
 			var tween = create_tween()
