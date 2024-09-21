81e298c2dde2eddeaa9a95bca286d742e935ca33
benkliator <benjaminekerna@gmail.com>
2024-09-21 12:05:20 +0200

    functional gameplay

--- serial-td/enemy/baseenemy.gd
+++ serial-td/enemy/baseenemy.gd
@@ -1,11 +1,17 @@
 extends PathFollow2D
 
 var speed = 150
+var hp = 2
 
 func _physics_process(delta: float) -> void:
 	_move(delta)
 	if get_progress() == 1:
-		queue_free()
+		get_parent().queue_free()
 
 func _move(delta: float) -> void:
 	set_progress(get_progress() + speed * delta)
+
+func take_damage(amount: int) -> void:
+	hp -= amount
+	if hp < 1:
+		queue_free()
--- serial-td/towers/base/baset1.gd
+++ serial-td/towers/base/baset1.gd
@@ -1,1 +1,8 @@
 extends "res://towers/turret.gd"
+
+#func _on_range_body_entered(body: Node2D) -> void:
+#	pass # Replace with function body.
+
+
+#func _on_range_body_exited(body: Node2D) -> void:
+#	pass # Replace with function body.
