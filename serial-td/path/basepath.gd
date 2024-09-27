25bd76c678ffa681cd34797eb20405016527ace8
benkliator <benjaminekerna@gmail.com>
2024-09-27 08:53:54 +0200

    infinite waves

--- serial-td/path/basepath.gd
+++ serial-td/path/basepath.gd
@@ -6,11 +6,10 @@ var timer
 # (Implement)
 var enemiesAlive = []
 var enemiesToSpawn = []
-var spawnWave = true
 var waitForEnemies = false
 
 var currentWave: int = 1
-var heat: int = 1
+var heat: float = 1
 
 func _ready() -> void:
 	enemiesToSpawn = gamedata.wave_data[currentWave]["enemies"]
@@ -18,27 +17,22 @@ func _ready() -> void:
 	add_child(timer)
 	timer.one_shot = false
 	timer.connect("timeout", _spawn_enemy)
-	timer.start(1.0)
+	timer.start(5.0)
 
 func _physics_process(delta: float) -> void:
-	if spawnWave:
-		spawnWave = false
-		timer.start(gamedata.wave_data[currentWave]["spawn_speed"])
-	if enemiesToSpawn.is_empty():
-		timer.stop()
-		waitForEnemies = true
-	if enemiesAlive.is_empty() and waitForEnemies:
+	if enemiesAlive.is_empty() and enemiesToSpawn.is_empty():
 		currentWave += 1
 		if currentWave > 10:
 			currentWave = 1
-			heat += 1
+			heat += 0.3
 		print("Current wave: ", currentWave)
-		enemiesToSpawn = gamedata.wave_data[currentWave].enemies
-		spawnWave = true
-		waitForEnemies = false
+		print("Heat: ", heat)
+		enemiesToSpawn = gamedata.wave_data[currentWave].enemies.duplicate()
+		timer.start(gamedata.wave_data[currentWave]["spawn_speed"] / heat)
 
 func _spawn_enemy() -> void:
 	if enemiesToSpawn.is_empty():
+		timer.stop()
 		return
 	var enemyName = enemiesToSpawn.pop_front()
 	var enemy = gamedata.enemies_from_string[enemyName].instantiate()
